5d0fdb0d125e32c6e06a97d2d182b4de3f452594
Sachiv007 <sachivray007@gmail.com>
2025-09-14 03:09:56 +0530

    Fix password reset flow

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://kuypxackmbyiaoxqvxka.supabase.co';   
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1eXB4YWNrbWJ5aWFveHF2eGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NTg0MDYsImV4cCI6MjA3MjEzNDQwNn0.nMJRWhA8iIhRdePoDZb1BPOnP6E8ucezxh_Vp_5pWMU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

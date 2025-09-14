973657023e57e41370e2ec50fe832bdac123059b
Sachiv007 <sachivray007@gmail.com>
2025-09-14 11:10:57 +0530

    test 7

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,9 @@ export default function Login() {
 
   const handleForgotPassword = async () => {
     if (!email) return alert("Enter your email to reset password.");
-    const { error } = await supabase.auth.resetPasswordForEmail(email);
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+  redirectTo: "http://localhost:5173/reset-password"
+});
 
     if (error) alert("Error sending reset email: " + error.message);
     else alert("Password reset email sent! Check your inbox.");
